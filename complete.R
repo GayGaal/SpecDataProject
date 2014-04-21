92f220166107e03eccee7bf9f2c6bee04f0931c4
Pavel Alimov <stvstalker@gmail.com>
2014-04-21 17:03:09 +0200

    changes

--- complete.R
+++ complete.R
@@ -12,16 +12,16 @@ complete <- function(directory, id = 1:332) {
         ## ...
         ## where 'id' is the monitor ID number and 'nobs' is the
         ## number of complete cases
-        result <- as.data.frame(matrix(0,ncol=2)) ##creating a set ncol data frame        
-        result <- result[-1,] ##deleting an empty unneeded row
-        for (i in id) { ##setting an observatoin cycle
-                fileName <- sprintf('%s/%03d.csv', directory, i) ##gets files names
-                rawData <-read.csv(fileName) ##reading the CSV file
-                nobs <- as.numeric(nrow(na.omit(rawData))) ##stores the number of complete observations for the ID
-                ##nrow could also use '(rawData[complete.cases(rawData),])' to calculate for data entries
+        result <- as.data.frame(matrix(0,ncol=2)) ## creating a set ncol data frame        
+        result <- result[-1,] ## deleting an empty unneeded row
+        for (i in id) { ## setting an observatoin cycle
+                fileName <- sprintf('%s/%03d.csv', directory, i) ## gets files names
+                rawData <-read.csv(fileName) ## reading the CSV file
+                nobs <- as.numeric(nrow(na.omit(rawData))) ## stores the number of complete observations for the ID
+                ## nrow could also use '(rawData[complete.cases(rawData),])' to calculate for data entries
                 newRow <- c(i,nobs) ## creating data for a row
-                result <- rbind(result, newRow) ##writing a new row of data
+                result <- rbind(result, newRow) ## writing a new row of data
         }
-        colnames(result) <- c("id", "nobs") ##assigning names to columns
+        colnames(result) <- c("id", "nobs") ## assigning names to columns
         result
 }
